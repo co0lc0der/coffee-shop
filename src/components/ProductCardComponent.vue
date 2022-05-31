04175710b50ea2404d677329e33680b3046fd347
co0lc0der <c0der@ya.ru>
2022-05-31 11:12:25 +0500

    add props

--- src/components/ProductCardComponent.vue
+++ src/components/ProductCardComponent.vue
@@ -1,9 +1,36 @@
 <template>
-	<div class="best__item">
-		<img src="@/assets/img/coffee-1.jpg" alt="coffee">
+	<div :class="classItem">
+		<img
+			:src="require(`@/assets/img/${image}`)"
+			:alt="image"
+		/>
 		<div class="best__item-title">
-			Solimo Coffee Beans 2kg
+			{{title}}
 		</div>
-		<div class="best__item-price">10.73$</div>
+		<div class="best__item-price">{{price}}$</div>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		classItem: {
+			type: String,
+			required: false,
+			default: "shop__item"
+		}
+	}
+}
+</script>
--- src/views/GoodsPageView.vue
+++ src/views/GoodsPageView.vue
@@ -37,12 +37,36 @@
 				<div class="row">
 					<div class="col-lg-10 offset-lg-1">
 						<div class="shop__wrapper">
-							<product-card-component />
-							<product-card-component />
-							<product-card-component />
-							<product-card-component />
-							<product-card-component />
-							<product-card-component />
+							<product-card-component
+								:title="products[0].title"
+								:price="products[0].price"
+								:image="products[0].image"
+							/>
+							<product-card-component
+								:title="products[1].title"
+								:price="products[1].price"
+								:image="products[1].image"
+							/>
+							<product-card-component
+								:title="products[2].title"
+								:price="products[2].price"
+								:image="products[2].image"
+							/>
+							<product-card-component
+								:title="products[3].title"
+								:price="products[3].price"
+								:image="products[3].image"
+							/>
+							<product-card-component
+								:title="products[4].title"
+								:price="products[4].price"
+								:image="products[4].image"
+							/>
+							<product-card-component
+								:title="products[5].title"
+								:price="products[5].price"
+								:image="products[5].image"
+							/>
 						</div>
 					</div>
 				</div>
