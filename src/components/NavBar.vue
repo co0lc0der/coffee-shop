484157196503ac83b0dbdc2b0ad103973c3c8531
co0lc0der <c0der@ya.ru>
2022-06-01 10:27:28 +0500

    add v-for to NavBar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,18 @@
 <template>
 	<header>
 		<ul class="header d-flex justify-content-center justify-content-md-start flex-wrap">
-			<li class="header__item">
-				<router-link :to="links[0].link">
-					<img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon">
-				</router-link>
-			</li>
+			<nav-link :link="links.header.link">
+				<img
+					:src="require(`@/assets/logo/${links.header.icon}`)"
+					:alt="links.header.icon"
+				/>
+			</nav-link>
+
 			<nav-link
-				:link="links[1].link"
-				:text="links[1].text"
-			/>
-			<nav-link
-				:link="links[2].link"
-				:text="links[2].text"
-			/>
-			<nav-link
-				:link="links[3].link"
-				:text="links[3].text"
+				v-for="link in links.other"
+				:key="link.id"
+				:link="link.link"
+				:text="link.text"
 			/>
 		</ul>
 	</header>
@@ -28,28 +24,30 @@ import NavLink from '@/components/NavLink.vue'
 export default {
 	data() {
 		return {
-			links: [
-				{
+			links: {
+				header: {
 					id: 0,
 					link: '/',
 					icon: 'Logo.svg'
 				},
-				{
-					id: 1,
-					link: '/our-coffee',
-					text: 'Our coffee',
-				},
-				{
-					id: 2,
-					link: '/goods-page',
-					text: 'For your pleasure',
-				},
-				{
-					id: 3,
-					link: '/contacts',
-					text: 'Contact us',
-				},
-			]
+				other: [
+					{
+						id: 1,
+						link: '/our-coffee',
+						text: 'Our coffee',
+					},
+					{
+						id: 2,
+						link: '/goods-page',
+						text: 'For your pleasure',
+					},
+					{
+						id: 3,
+						link: '/contacts',
+						text: 'Contact us',
+					},
+				]
+			}
 		}
 	},
 	components: {
--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,6 +1,6 @@
 <template>
 	<li :class="classItem">
-		<router-link :to="link">{{ text }}</router-link>
+		<router-link :to="link">{{ text }}<slot></slot></router-link>
 	</li>
 </template>
 
