5650b74a2ebc30cf87cf69b608daa89984763473
co0lc0der <c0der@ya.ru>
2022-06-02 11:08:20 +0500

    add v-model to contact form

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -17,8 +17,8 @@
 					<div class="col col-12 col-lg-6 offset-0 offset-lg-3">
 						<div class="title mt-5">Tell us about your tastes</div>
 						<img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
-	
-						<form action="#" class="mt-5">
+
+						<form action="#" class="mt-5" @submit.prevent="submit">
 							<div class="form-group row">
 								<div class="col col-12 col-sm-3 d-flex align-items-center">
 									<label for="name-input" class="mb-0">
@@ -27,10 +27,10 @@
 									</label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input type="text" class="form-control" id="name-input">
+									<input v-model="form.name" type="text" class="form-control" id="name-input">
 								</div>
 							</div>
-	
+
 							<div class="form-group row">
 								<div class="col col-12 col-sm-3 d-flex align-items-center">
 									<label for="email-input" class="mb-0">
@@ -39,10 +39,10 @@
 									</label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input type="email" class="form-control" id="email-input">
+									<input v-model="form.email" type="email" class="form-control" id="email-input">
 								</div>
 							</div>
-	
+
 							<div class="form-group row">
 								<div class="col col-12 col-sm-3 d-flex align-items-center">
 									<label for="phone-input" class="mb-0">
@@ -50,10 +50,10 @@
 									</label>
 								</div>
 								<div class="col col-12 col-sm-9">
-									<input type="tel" class="form-control" id="phone-input">
+									<input v-model="form.tel" type="tel" class="form-control" id="phone-input">
 								</div>
 							</div>
-	
+
 							<div class="form-group row textarea">
 								<div class="col col-12 d-flex justify-content-center">
 									<label for="pmessage" class="mb-3 mt-3 text-center">
@@ -62,14 +62,14 @@
 									</label>
 								</div>
 								<div class="col col-12">
-									<textarea class="form-control" name="message" id="message" rows="5"
-										placeholder="Leave your comments here"></textarea>
+									<textarea v-model="form.message" class="form-control" name="message" id="message"
+										rows="5" placeholder="Leave your comments here"></textarea>
 								</div>
 							</div>
-	
+
 							<div class="row">
 								<div class="col">
-									<button class="btn btn-outline-dark send-btn">Send us</button>
+									<button type="submit" class="btn btn-outline-dark send-btn">Send us</button>
 								</div>
 							</div>
 						</form>
@@ -88,6 +88,21 @@ export default {
 	components: {
 		NavBar,
 		HeaderTitle
+	},
+	data() {
+		return {
+			form: {
+				name: '',
+				email: '',
+				tel: '',
+				message: '',
+			}
+		}
+	},
+	methods: {
+		submit() {
+			console.log(this.form);
+		}
 	}
 }
 </script>
